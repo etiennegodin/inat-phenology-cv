d3be77056ef692a6031083fffb89c2dd33fc7040
etiennegodin <etiennegodin@duck.com>
2026-04-27 10:01:17 -0400

    new head with dropout

--- src/pytorch_pipeline/model.py
+++ src/pytorch_pipeline/model.py
@@ -7,7 +7,9 @@ def build_model() -> nn.Module:
     in_features = model.fc.in_features
     for p in model.parameters():
         p.requires_grad = False
-    model.fc = nn.Linear(in_features, 1)
+    model.fc = nn.Sequential(
+        nn.Linear(in_features, 256), nn.ReLU(), nn.Dropout(0.5), nn.Linear(256, 1)
+    )
     return model
 
 
