f9be3b4663b6925d837d2136708eadd1857011ee
etiennegodin <etiennegodin@duck.com>
2026-04-28 13:42:36 -0400

    params and config split

--- src/pytorch_pipeline/config.py
+++ src/pytorch_pipeline/config.py
@@ -6,42 +6,14 @@ from torch.utils.data import DataLoader
 
 from .dataset import build_datasets
 from .model import build_model, get_backbone
-
-
-@dataclass
-class TrainingParams:
-    epochs: int = 3
-    patience: int = 3
-
-
-@dataclass
-class OptimizerParams:
-    learning_rate: float = 0.001
-
-
-@dataclass
-class ModelParams:
-    head_inputs: int = 256
-    dropout_prob: float = 0.5
-
-
-@dataclass
-class DataLoadersParams:
-    batch_size: int = 16
-
-
-@dataclass
-class PathsParams:
-    root: str
-    checkpoint_path: str
-    db_path: str
-
-
-@dataclass
-class SamplesParams:
-    observations_col: str = "observation_id"
-    photo_id_col: str = "photo_id"
-    label_col: str = "controlled_value_id"
+from .utils.params import (
+    DataLoadersParams,
+    ModelParams,
+    OptimizerParams,
+    PathsParams,
+    SamplesParams,
+    TrainingParams,
+)
 
 
 @dataclass
---
+++ src/pytorch_pipeline/utils/params.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class TrainingParams:
+    epochs: int = 3
+    patience: int = 3
+
+
+@dataclass
+class OptimizerParams:
+    learning_rate: float = 0.001
+
+
+@dataclass
+class ModelParams:
+    head_inputs: int = 256
+    dropout_prob: float = 0.5
+
+
+@dataclass
+class DataLoadersParams:
+    batch_size: int = 16
+
+
+@dataclass
+class PathsParams:
+    root: str
+    checkpoint_path: str
+    db_path: str
+
+
+@dataclass
+class SamplesParams:
+    observations_col: str = "observation_id"
+    photo_id_col: str = "photo_id"
+    label_col: str = "controlled_value_id"
