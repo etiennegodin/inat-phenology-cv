909047a1ea23240cd2234d920e4969f12e617abf
etiennegodin <etiennegodin@duck.com>
2026-08-04 22:26:31 -0400

    Logging images stats

--- src/pytorch_pipeline/train/workflow.py
+++ src/pytorch_pipeline/train/workflow.py
@@ -5,6 +5,7 @@ import time
 from typing import TYPE_CHECKING, Any
 
 import mlflow
+import numpy as np
 import torch
 from torch.amp import autocast_mode, grad_scaler
 
@@ -48,7 +49,24 @@ def train_one_epoch(
     total_loss = 0
     model.train()
     t0 = time.time()
+
+    batches = 0
+    img_per_batch = []
+    obs_per_batch = []
+
     for images, labels in dataloader:
+        indices = []
+        for i in images:
+            indices.append(i.size()[0])
+
+        # Stats
+        total_img = sum(indices)
+        obs = len(indices)
+        obs_per_batch.append(obs)
+        img_per_batch.append(total_img)
+
+        batches += 1
+
         t1 = time.time()
         data_time += t1 - t0
 
@@ -82,6 +100,17 @@ def train_one_epoch(
 
     train_loss = total_loss / len(dataloader)
 
+    print("total_images", np.sum(img_per_batch))
+    print("total_batches", batches)
+    print("total obs", len(dataloader))
+
+    print(f"observation per batch mean {np.mean(obs_per_batch):.3f}")
+    print(f"observation per batch std {np.std(obs_per_batch):.3f}")
+    print(f"imgs per batch mean {np.mean(img_per_batch):.3f}")
+    print(f"imgs per batch std {np.std(img_per_batch):.3f}")
+
+    print(f"imgs per observation mean {(np.sum(img_per_batch) / len(dataloader)):.3f}")
+
     # MLflow 'step' tells it which epoch this is for the graph
     mlflow.log_metric("train_loss", train_loss, step=epoch)
 
@@ -94,7 +123,12 @@ def evaluate(
     criterion: nn.Module,
     device: device,
     epoch: int,
-) -> dict[str, Any]:
+) -> tuple[dict[str, Any], tuple[float, float]]:
+
+    data_time = 0
+    compute_time = 0
+    t0 = time.time()
+
     total_loss = 0
     correct = 0
     total = 0
@@ -102,16 +136,22 @@ def evaluate(
     all_labels = []
     all_preds_raw = []
     model.eval()
+
     with torch.no_grad():
         for images, labels in dataloader:
             labels: Tensor
             images = [t.to(device) for t in images]
             labels = labels.to(device)
 
+            t1 = time.time()
+            data_time += t1 - t0
+
             # Run foward prop and backprop
             if device.type == "cuda":
                 with autocast_mode.autocast(device_type=device.type):
                     predictions, weights = model(images)
+                    torch.cuda.synchronize()
+
             else:
                 predictions, weights = model(images)
 
@@ -126,6 +166,8 @@ def evaluate(
             all_preds.append(preds)
             all_labels.append(labels)
             all_preds_raw.append(preds_raw)
+            t0 = time.time()
+            compute_time += t0 - t1
 
     # Convert back to numpy arrays for metrics
     all_preds = torch.cat(all_preds).detach().cpu().numpy()
@@ -140,7 +182,7 @@ def evaluate(
 
     eval_metrics = log_metrics(all_preds, all_labels, all_preds_raw, epoch)
     eval_metrics.update(base_metrics)
-    return eval_metrics
+    return eval_metrics, (data_time, compute_time)
 
 
 def execute(
@@ -166,7 +208,7 @@ def execute(
             print(f"Skipping epoch {epoch}")
             continue
 
-        train_loss, times = train_one_epoch(
+        train_loss, train_times = train_one_epoch(
             epoch,
             model=model,
             dataloader=train_loader,
@@ -184,25 +226,34 @@ def execute(
         mlflow.log_metric("lr_backbone", float(current_lr[0]), step=epoch)
         mlflow.log_metric("lr_attention", float(current_lr[1]), step=epoch)
         mlflow.log_metric("lr_head", float(current_lr[2]), step=epoch)
-        mlflow.log_metric("train_data_time", float(times[0]), step=epoch)
-        mlflow.log_metric("train_compute_time", float(times[1]), step=epoch)
+        mlflow.log_metric("train_data_time", float(train_times[0]), step=epoch)
+        mlflow.log_metric("train_compute_time", float(train_times[1]), step=epoch)
 
-        eval_metrics = evaluate(
+        eval_metrics, eval_times = evaluate(
             model=model,
             dataloader=val_loader,
             criterion=criterion,
             device=device,
             epoch=epoch,
         )
+
         val_loss = eval_metrics["val_loss"]
         gap = val_loss - train_loss
         mlflow.log_metric("loss_gap", gap, step=epoch)
+        mlflow.log_metric("eval_data_time", float(eval_times[0]), step=epoch)
+        mlflow.log_metric("eval_compute_time", float(eval_times[1]), step=epoch)
 
         print(
-            f"Epoch {epoch}: train={train_loss:.3f} val={eval_metrics['val_loss']:.3f} "
-            f"gap={gap:.3f} "
+            f"Epoch {epoch}: train={train_loss:.6f} val={eval_metrics['val_loss']:.6f} "
+            f"gap={gap:.6f} "
             f"roc_auc_macro={float(eval_metrics['val_roc_auc_macro']):.3f} "
-            f"data_time={times[0]:.1f}s compute_time={times[1]:.1f}s"
+        )
+
+        print(
+            f"train data_time={train_times[0]:.1f}s | "
+            f"train_compute_time={train_times[1]:.1f}s | "
+            f"eval data_time={eval_times[0]:.1f}s | "
+            f"eval_compute_time={eval_times[1]:.1f}s"
         )
 
         if device.type == "cuda" and epoch == 0:
